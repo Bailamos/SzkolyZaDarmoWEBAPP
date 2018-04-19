7451590a51b55f962f020fa15d14af95537466d3
Bailamos <bailamos@github.com>
2018-04-20 01:16:26 +0200

    error color

--- src/app/user/training-entry/training-entry-form/training-entry-form.component.ts
+++ src/app/user/training-entry/training-entry-form/training-entry-form.component.ts
@@ -55,7 +55,7 @@ export class TrainingEntryFormComponent extends FormComponent implements OnInit 
             'email': new FormControl(null, [Validators.required, Validators.email]),
             'name': new FormControl(null, [Validators.required, Validators.pattern("^[a-zA-Z\\s]*$")]),
             'surname': new FormControl(null, [Validators.required, Validators.pattern("^[a-zA-Z\\s]*$")]),
-            'birthYear': new FormControl(null, Validators.required),
+            'birthYear': new FormControl(null, [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]),
             'marketStatus': new FormControl(this.marketStatuses[0].id, Validators.required),
             'education': new FormControl(this.educations[0].id, Validators.required),
             'areaOfResidence': new FormControl(this.areasOfResidence[0].id, Validators.required),
